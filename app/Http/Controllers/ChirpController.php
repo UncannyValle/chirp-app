22a83991d602e4b73457cfe7b8f7527252fa5dad
Julian Valle <jvalle092@gmail.com>
2024-02-15 14:53:01 -0800

    create and update completed

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,34 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChirpRequest;
 use App\Models\Chirp;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
         return Inertia::render('Chirps/Index', [
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get()
         ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ChirpRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
     {
         //
     }
@@ -53,9 +59,17 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(ChirpRequest $request, Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('update', $chirp); //allows the current user to update the chirp
+
+        // this takes in the request and validates it to make sure it is a string,
+        // not null and max255 chars
+        $validated = $request->validated();
+        
+        $chirp->update($validated); //then it actually runs the route and updates the chirp taking in the validated role
+
+        return redirect(route('chirps.index')); //finally it redirects the user to the chirps/index page
     }
 
     /**
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,4 +1,4 @@
-import { Config } from 'ziggy-js';
+import {Config} from 'ziggy-js';
 
 export interface User {
     id: number;
@@ -7,9 +7,20 @@ export interface User {
     email_verified_at: string;
 }
 
+export interface Chirp {
+    id: string;
+    message: string;
+    user: User;
+    created_at: string;
+    updated_at: string;
+}
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
     };
     ziggy: Config & { location: string };
+    chirps: [
+        chirp: Chirp
+    ];
 };
